f37e3c3fa1b3dbc7843c9cc2c4c5879ed054c069
Ahmed Zakaria <ahmed.zakaria.sera@gmail.com>
2025-01-13 13:53:17 +0200

    layout and outlet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import FormModal from "./components/FormModal";
 import Login from "./components/Auth/Login";
 import MessagesViewer from "./components/MessagesViewer";
 import ProtectRouter from "./components/ProtectRouter";
-import { Outlet } from "react-router-dom"; // For child route rendering
+import Layout from "./components/Layout"; // Assuming Layout is your parent component
 
 export default function App() {
   return (
     <>
       <BrowserRouter basename="/Engagement">
-        {" "}
         {/* Set basename for GH Pages */}
         <Routes>
-          <Route path="/" element={<Home />}>
-            {/* Define child routes under Home */}
-            <Route path="/private-login-boyka-fulla" element={<Login />} />
+          {/* Parent route (Layout) */}
+          <Route path="/" element={<Layout />}>
+            {/* Child routes (relative paths) */}
+            <Route index element={<Home />} />
+            <Route path="private-login-boyka-fulla" element={<Login />} />
             <Route
-              path="/messageViewer"
+              path="messageViewer"
               element={
                 <ProtectRouter>
                   <MessagesViewer />
